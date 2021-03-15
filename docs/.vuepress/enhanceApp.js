3fb4338970976b042c00ae43cf91ccaffd898620
Warren Anderson <warren@wyomingsoftware.com>
2021-03-15 07:03:47 -0600

    Add enhanceApp file loaded at VuePress startup

---
+++ docs/.vuepress/enhanceApp.js
@@ -0,0 +1,4 @@
+export default ({ Vue, router, options }) => {
+  //router.addRoute({ path: '/next', redirect: '/next/grp-providers/'})
+  console.log('enhanceApp.js > router:',router.getRoutes())
+}
