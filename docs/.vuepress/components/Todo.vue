d1f9faaf522cea9f17339a9cf14fdde76a05369d
Warren Anderson <warren@wyomingsoftware.com>
2021-04-29 16:31:06 -0600

    New Todo component. Inserts a slot component into markdown to areas of concern. Should only be used in the /next release for as a marker for improvements.

---
+++ docs/.vuepress/components/Todo.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+      <!-- This hidden block calculates the running badge total for the page -->
+      <span style="display:none;">
+      {{$page.todoCnt}} 
+      {{$page.todoCnt = $page.todoCnt + 1}}
+      {{$page.todoCnt}}
+      </span>
+
+      <!-- badge total fixed on top left -->
+      <span class="badgeCnt"><Badge
+        type="tip" :text="'Todos: '+$page.todoCnt" 
+        vertical="top" />
+      </span>
+
+      <!-- The slot -->
+      <div  class="borderRed">
+        <span><Badge 
+          style="padding-top:0px;backgroundColor:red;" 
+          type="tip" :text="'Todo: '+$page.todoCnt" 
+          vertical="top" />
+        </span>
+        <a v-show="githubID" :href="githubURL" target="github">
+          GitHub Issue: #{{githubID}}
+        </a>
+        <div style="margin-top:-10px;margin-bottom:-10px;"><slot>Todo:</slot></div>
+      </div>
+  </div>
+</template>
+
+<script>
+
+
+  export default {
+      name: 'todo',
+      props: {githubID:Number
+    },
+    data: () => ({
+      githubURL:String,
+    }),
+    mounted() {
+      this.$nextTick(function () {
+        this.$page.todoCnt = 0 // Needed here and beforeMount
+        this.githubURL = "https://github.com/api3dao/api3-docs/issues/"+this.githubID
+      })
+    },
+    beforeUpdate() {
+      this.$nextTick(function () {
+        this.$page.todoCnt = 0 // Needed here and mounted
+        //console.log('> beforeUpdate', this.$page)
+      })
+    }
+  }
+</script>
+
+<style scoped>
+
+.borderRed{
+  padding-top:5px;
+  color:#404040;
+  border:dotted red 2px;
+  padding-left:5px;
+}
+
+.badgeCnt {
+  position:fixed;
+  top:59px;
+  right:10px;
+  z-index:999;
+}
+
+a{
+  float:right;
+  padding-right:20px;
+}
+
+</style>
