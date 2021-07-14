fee9ce26b5360a7309030358c769303c31e5454b
Warren Anderson <warren@wyomingsoftware.com>
2021-07-14 16:06:56 -0600

    Problem with new redirect Works on dev but not prod

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -2,12 +2,14 @@ export default ({ Vue, router, options }) => {
 
   router.addRoutes([
 
-    // Takes the user to the current release
-    { path: '/latest', redirect: '/pre-alpha' },
-
     // So the DAO dashboard can find its way to the current release for the role API3 members
+    { path: '/latest/members/', redirect: '/pre-alpha/members' },
     { path: '/latest/members', redirect: '/pre-alpha/members' },
-
+    
+    // Takes the user to the current release
+    { path: '/latest/', redirect: '/pre-alpha' },
+    { path: '/latest', redirect: '/pre-alpha' },
+    
     // "/technology" has been changed to "/reference"
     { path: '/next/technology/*', redirect: '/next' }
   ])
