ed0ea9a203e769941c0c219395d7bbe248a4bfc3
Warren Anderson <warren@wyomingsoftware.com>
2021-10-24 14:23:18 -0600

    Add blank validator docs

--- docs/airnode/v0.2/sidebar.js
+++ docs/airnode/v0.2/sidebar.js
@@ -1,63 +1,84 @@
 module.exports = [
   {
-    title: 'Introduction', initialOpenGroupIndex: 0, collapsable: true,
-    children: [
-      '',
-      'introduction/why-use-airnode'
-    ]
+    title: 'Introduction',
+    initialOpenGroupIndex: 0,
+    collapsable: true,
+    children: ['', 'introduction/why-use-airnode'],
   },
   {
-    title: 'API Providers', initialOpenGroupIndex: 0, collapsable: false, 
+    title: 'API Providers',
+    initialOpenGroupIndex: 0,
+    collapsable: false,
     children: [
       'grp-providers/', // Routes to the README.md in grp-providers
-      {title:'Design Philosophy', path:'grp-providers/airnode/design-philosophy'},
-            {title:'Implementation', path:'grp-providers/airnode/implementation'},
-            {title:'Ethereum Providers', path:'grp-providers/airnode/ethereum-providers'},
       {
-        title: 'Build an Airnode', collapsable: true, 
-        children:[
+        title: 'Design Philosophy',
+        path: 'grp-providers/airnode/design-philosophy',
+      },
+      { title: 'Implementation', path: 'grp-providers/airnode/implementation' },
+      {
+        title: 'Ethereum Providers',
+        path: 'grp-providers/airnode/ethereum-providers',
+      },
+      {
+        title: 'Build an Airnode',
+        collapsable: true,
+        children: [
           'grp-providers/guides/build-an-airnode/',
           'grp-providers/guides/build-an-airnode/api-integration',
           'grp-providers/guides/build-an-airnode/configuring-airnode',
           'grp-providers/guides/build-an-airnode/apply-auth',
-          {title:'Heartbeat (optional)', path:'grp-providers/guides/build-an-airnode/heartbeat'},
-          {title:'HTTP Gateway (optional)', path:'grp-providers/guides/build-an-airnode/http-gateway'},
+          {
+            title: 'Heartbeat (optional)',
+            path: 'grp-providers/guides/build-an-airnode/heartbeat',
+          },
+          {
+            title: 'HTTP Gateway (optional)',
+            path: 'grp-providers/guides/build-an-airnode/http-gateway',
+          },
           'grp-providers/guides/build-an-airnode/deploying-airnode',
-        ]
+        ],
       },
       {
-        title: 'Docker Images', collapsable: true, 
-        children:[
+        title: 'Docker Images',
+        collapsable: true,
+        children: [
           'grp-providers/docker/',
           'grp-providers/docker/deployer-image',
           'grp-providers/docker/client-image',
-        ]
+        ],
       },
       {
-        title: 'Tutorial', collapsable: true,
+        title: 'Tutorial',
+        collapsable: true,
         children: [
           'grp-providers/tutorial/',
           'grp-providers/tutorial/config-json',
           'grp-providers/tutorial/secrets-env',
           'grp-providers/tutorial/aws-env',
-        ]
-      }
-    ]
+        ],
+      },
+      'grp-providers/validator',
+    ],
   },
   {
-    title: 'Developers', initialOpenGroupIndex: 0, collapsable: false,
-    children:[
+    title: 'Developers',
+    initialOpenGroupIndex: 0,
+    collapsable: false,
+    children: [
       'grp-developers/', // Routes to the README.md in grp-developers
       'grp-developers/requesters-sponsors',
       'grp-developers/call-an-airnode',
       'grp-developers/using-templates',
       'grp-developers/fees',
       'grp-developers/self-serve-platforms',
-    ]
+    ],
   },
   {
-    title: 'Concepts and Definitions', collapsable: true, initialOpenGroupIndex: -1,
-    children:[
+    title: 'Concepts and Definitions',
+    collapsable: true,
+    initialOpenGroupIndex: -1,
+    children: [
       'concepts/', // Routes to the README.md in /concepts
       'concepts/airnode',
       'concepts/endpoint',
@@ -68,51 +89,66 @@ module.exports = [
       'concepts/sponsor',
       'concepts/template',
       'concepts/request',
-      'concepts/chain-providers'
+      'concepts/chain-providers',
     ],
   },
   {
-    title: 'Reference', initialOpenGroupIndex: -1, collapsable: true,
+    title: 'Reference',
+    initialOpenGroupIndex: -1,
+    collapsable: true,
     children: [
       {
-        title: 'Specifications', collapsable: true,
+        title: 'Specifications',
+        collapsable: true,
         children: [
-            {title:'Oracle Integration Specifications (OIS)', path:'reference/specifications/ois'},
-            'reference/specifications/airnode-abi-specifications',
-            'reference/specifications/reserved-parameters'
-        ]
-      },
-      {
-        title: 'Deployment Files', collapsable: true,
-        children: [
-            {title:'Overview', path:'reference/deployment-files/overview'},
-            {title:'config.json', path:'reference/deployment-files/config-json'},
-            {title:'secrets.env', path:'reference/deployment-files/secrets-env'},
-            {title:'aws.env', path:'reference/deployment-files/aws-env'},
-            {title:'receipt.json', path:'reference/deployment-files/receipt-json'}
-            
+          {
+            title: 'Oracle Integration Specifications (OIS)',
+            path: 'reference/specifications/ois',
+          },
+          'reference/specifications/airnode-abi-specifications',
+          'reference/specifications/reserved-parameters',
         ],
       },
       {
-        title: 'Examples Files', collapsable: true,
+        title: 'Deployment Files',
+        collapsable: true,
         children: [
-            'reference/examples/config-json',
-            'reference/examples/secrets-env',
-            'reference/examples/aws-env'
-            
+          { title: 'Overview', path: 'reference/deployment-files/overview' },
+          {
+            title: 'config.json',
+            path: 'reference/deployment-files/config-json',
+          },
+          {
+            title: 'secrets.env',
+            path: 'reference/deployment-files/secrets-env',
+          },
+          { title: 'aws.env', path: 'reference/deployment-files/aws-env' },
+          {
+            title: 'receipt.json',
+            path: 'reference/deployment-files/receipt-json',
+          },
+        ],
+      },
+      {
+        title: 'Examples Files',
+        collapsable: true,
+        children: [
+          'reference/examples/config-json',
+          'reference/examples/secrets-env',
+          'reference/examples/aws-env',
         ],
       },
       {
         title: 'Templates',
-        children:[
+        children: [
           'reference/templates/ois-json',
           'reference/templates/config-json',
           'reference/templates/secrets-env',
-          'reference/templates/aws-env'
-        ]
+          'reference/templates/aws-env',
+        ],
       },
       'reference/admin-cli-commands',
       'reference/airnode-addresses',
-    ]
-  }
-]
+    ],
+  },
+];
