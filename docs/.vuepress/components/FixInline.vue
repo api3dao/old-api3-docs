b91e3b8a3a3e5acc98daca24525105a9ea4b769f
Warren Anderson <warren@wyomingsoftware.com>
2021-10-27 12:28:11 -0600

    Changes to accomidate prettier

--- docs/.vuepress/components/FixInline.vue
+++ docs/.vuepress/components/FixInline.vue
@@ -1,22 +1,26 @@
 <!--
+Fix.vue and FixInline.vue are marker components used as a placeholder for items 
+that need to be resolved inside a document. These are not meant to be used 
+in production docs. Use only for /next.
 -->
 
 <template>
   <span class="slot" >
-      <span style="font-weight:bold;font-size:medium;">></span> <slot></slot>
+      <span> > {{content}} < </span>
   </span>
 </template>
 
 <script>
   export default {
-    name: 'fix-inline'
+    name: 'fix-inline',
+    props: ['content']
   }
 </script>
 
 <style scoped>
   .slot{
     padding-left:10px;
-    font-size:small;
+    font-size:medium;
     color:red;
   }
 
