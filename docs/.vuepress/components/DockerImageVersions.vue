30d1714014228b2f06da91afbc84813214beab82
Warren Anderson <warren@api3.org>
2021-11-05 10:09:43 -0600

    New component for Docker Shows the available Docker image versions

---
+++ docs/.vuepress/components/DockerImageVersions.vue
@@ -0,0 +1,53 @@
+<!--
+-->
+
+<template>
+  <span>
+    <ul>
+      <li>
+        <a
+          :href="'https://hub.docker.com/r/api3/airnode-deployer/tags'"
+          target="_docker-hub"
+          >Airnode deployer images
+          <ExternalLinkImage />
+        </a>
+      </li>
+
+      <li>
+        <a
+          :href="'https://hub.docker.com/r/api3/airnode-client/tags'"
+          target="_docker-hub"
+          >Airnode client images
+          <ExternalLinkImage />
+        </a>
+      </li>
+    </ul>
+
+    <p>
+      The deployer and client images must pair with a particular
+      <code>nodeVersion</code>
+      from your config.json file. However there is a
+      <code>latest</code> version provided for convenience which always requires
+      the highest <code>nodeVersion</code> available at the time of its
+      execution.
+    </p>
+
+    <table>
+      <tr>
+        <th>Deployer or Client Image</th>
+        <th>nodeVersion</th>
+      </tr>
+      <tr>
+        <td>0.2.2</td>
+        <td>0.2.2</td>
+      </tr>
+      <tr>
+        <td>latest</td>
+        <td>0.2.2</td>
+      </tr>
+    </table>
+    <div style="font-size: small; margin-left: 20px; margin-top: -10px">
+      The above table updates when new images are released.
+    </div>
+  </span>
+</template>
