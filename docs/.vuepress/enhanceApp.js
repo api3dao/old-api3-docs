6c21b40867bd115d5b4bdb4c2bee7e3662dc3817
Warren Anderson <warren@api3.org>
2021-12-03 06:55:22 -0700

    Setup for AN-415 This will provide for the re-write of any URL using the part `/airnode/latest/`

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,5 +1,19 @@
 export default ({ Vue, router, options }) => {
-  //router.addRoutes([
-  //])
-  //console.log('enhanceApp.js > router:',router.getRoutes())
+  //router.addRoutes([]);
+  /**
+   * This is an early look at AN-415 that can be implemented here by
+   * using a Vue global guard.
+   * https://router.vuejs.org/guide/advanced/navigation-guards.html#global-before-guards
+   */
+  /*router.beforeEach((to, from, next) => {
+    if (to.path.indexOf('/airnode/latest/') > -1) {
+      console.log('request using latest >', to.path);
+      const newRoute = to.path.replace('/airnode/latest/', '/airnode/v0.3/');
+      console.log('send to >', newRoute);
+      next({ path: newRoute });
+    } else {
+      next();
+    }
+  });
+  */
 };
