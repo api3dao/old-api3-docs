9c60feb05405d922009c1249f16f99fc8c8a8653
Warren Anderson <warren@api3.org>
2022-02-16 05:52:36 -0700

    Removed console output msg

--- docs/.vuepress/components/Video.vue
+++ docs/.vuepress/components/Video.vue
@@ -37,9 +37,7 @@ export default {
     },
   },
   mounted() {
-    console.log('SRC >', this.src);
     this.setPlayerSize();
-
     window.addEventListener('resize', () => {
       this.setPlayerSize();
     });
