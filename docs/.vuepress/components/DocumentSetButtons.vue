289e51a643e16d7939c3083f4a23f3468459d8d0
Derek Croote <dcroote@users.noreply.github.com>
2022-03-21 19:15:04 -0700

    Center landing page doc sets and make imgs links

--- docs/.vuepress/components/DocumentSetButtons.vue
+++ docs/.vuepress/components/DocumentSetButtons.vue
@@ -8,17 +8,19 @@ it will always default to the startPath in config.json.
 <template>
   <div class="landing-container">
     <li>
-      <img src="/img/01-Airnode-State=Active@2x.png" />
-      <router-link class="nav-link" :to="latestVersion">Airnode</router-link>
+      <router-link :to="latestVersion">
+        <img src="/img/01-Airnode-State=Active@2x.png" />
+        Airnode</router-link
+      >
       <p>
         First-party serverless oracle node that solves the Web3 API connectivity
         problem
       </p>
     </li>
     <li>
-      <img src="/img/Beacons-active.png" />
-      <router-link class="nav-link" :to="latestBeaconVersion"
-        >Beacons</router-link
+      <router-link :to="latestBeaconVersion">
+        <img src="/img/Beacons-active.png" />
+        Beacons</router-link
       >
       <p>
         First-party Web3 data feeds powering transparent, reliable, and
@@ -26,21 +28,27 @@ it will always default to the startPath in config.json.
       </p>
     </li>
     <li>
-      <img src="/img/ois-active.png" />
-      <router-link class="nav-link" :to="latestOisVersion">OIS</router-link>
+      <router-link :to="latestOisVersion">
+        <img src="/img/ois-active.png" />
+        OIS</router-link
+      >
       <p>
         Oracle Integration Specifications - linking Airnode endpoints to API
         operations
       </p>
     </li>
     <li>
-      <img src="/img/02-DAO-State=Active@2x-1.png" />
-      <router-link class="nav-link" to="/dao-members/">DAO Members</router-link>
+      <router-link to="/dao-members/">
+        <img src="/img/02-DAO-State=Active@2x-1.png" />
+        DAO Members</router-link
+      >
       <p>API3 DAO structure, functionality, and resources</p>
     </li>
     <li>
-      <img src="/img/03-API3-State=Active@2x-2.png" />
-      <router-link class="nav-link" to="/api3/">API3 Overview</router-link>
+      <router-link to="/api3/">
+        <img src="/img/03-API3-State=Active@2x-2.png" />
+        API3 Overview</router-link
+      >
       <p>Resources for learning more about the project and its mission</p>
     </li>
   </div>
@@ -64,7 +72,6 @@ export default {
 </script>
 
 <style scoped>
-/* "nav-link" are from the VuePress classes. */
 .landing-container {
   display: flex;
   list-style: none;
@@ -73,13 +80,14 @@ export default {
 }
 .landing-container img {
   display: block;
-  margin: 0.5rem 0px;
+  margin: 0.5rem auto;
   width: 64px;
 }
 .landing-container li {
   flex-basis: 160px;
   flex-grow: 1;
   padding: 1rem;
+  text-align: center;
 }
 .landing-container a {
   font-size: 1.2rem;
