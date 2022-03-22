38162f175e4747b215cb8e3a773e82b0563ee80a
John Chen <czhttp@gmail.com>
2022-03-22 15:19:24 +0800

    New translations sidebar.js (Chinese Simplified)

---
+++ docs/ois/zh/sidebar.js
@@ -0,0 +1,12 @@
+module.exports = [{
+        title: '',
+        initialOpenGroupIndex: 1,
+        collapsable: false,
+        children: [
+            '',
+            'ois.md',
+            'reserved-parameters',
+            'example.md',
+            'ois-template.md'
+        ]
+    }];
