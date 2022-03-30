fe0659e7c0833a85439f2bf15d808626db09354c
John Chen <czhttp@gmail.com>
2022-03-31 00:32:29 +0800

    New translations Divider.vue (Chinese Simplified)

---
+++ docs/.vuepress/components/zh/Divider.vue
@@ -0,0 +1,20 @@
+<!--
+-->
+
+<template>
+  <div>
+    <hr />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'divider',
+};
+</script>
+
+<style scoped>
+hr {
+  border-top: solid 2px gray;
+}
+</style>
