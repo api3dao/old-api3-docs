b80409f0137ab54410449041d3d398eac5673c2e
John Chen <czhttp@gmail.com>
2022-03-31 00:32:06 +0800

    New translations SponsorWalletWarning.vue (Chinese Simplified)

---
+++ docs/.vuepress/components/airnode/zh/SponsorWalletWarning.vue
@@ -0,0 +1,21 @@
+<!--
+This is a POST pre-alpha component. Notice the use of 
+"sponsor" and not "requester" related to the designated wallet.
+
+Use only in version >= v0.2
+
+WARNING: Do not place HTML comment lines inside paragraph elements.
+-->
+
+<template>
+  <div class="custom-block warning">
+    <p class="custom-block-title">Designated Sponsor Wallets</p>
+    <p>
+      Sponsors should not fund a <code>sponsorWallet</code> with more then they
+      can trust the Airnode with, as the Airnode controls the private key to the
+      <code>sponsorWallet</code>. The deployer of such Airnode undertakes no
+      custody obligations, and the risk of loss or misuse of any excess funds
+      sent to the <code>sponsorWallet</code> remains with the sponsor.
+    </p>
+  </div>
+</template>
