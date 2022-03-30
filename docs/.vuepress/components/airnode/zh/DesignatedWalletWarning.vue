c632f065da9931e81b6716e13f2ba1b7470be1ed
John Chen <czhttp@gmail.com>
2022-03-31 00:32:05 +0800

    New translations DesignatedWalletWarning.vue (Chinese Simplified)

---
+++ docs/.vuepress/components/airnode/zh/DesignatedWalletWarning.vue
@@ -0,0 +1,19 @@
+<!--
+This is ONLY a pre-alpha component. Notice the use of 
+"requester" and not "sponsor" related to the designated wallet.
+
+WARNING: Do not place HTML comment lines inside paragraph elements.
+-->
+
+<template>
+  <div class="custom-block warning">
+    <p class="custom-block-title">Designated Wallets</p>
+    <p>
+      Requesters should not fund a designated wallet with more then they can
+      trust the Airnode with, as the Airnode controls the private key to the
+      designated wallet. The deployer of such Airnode undertakes no custody
+      obligations, and the risk of loss or misuse of any excess funds sent to
+      the designated wallet remains with the requester.
+    </p>
+  </div>
+</template>
