ae60f4af27a49c81cd1b1e54ab12fdb6ffeac926
Warren Anderson <warren@api3.org>
2022-05-13 14:05:50 -0600

    Use newer gen2 search box

--- docs/.vuepress/components/Navbar.vue
+++ docs/.vuepress/components/Navbar.vue
@@ -53,15 +53,17 @@
       <!-- Added: wkande: This adds the custom Versions component. -->
       <Versions />
 
-      <AlgoliaSearchBox v-if="isAlgoliaSearch" :options="algolia" />
+      <search-SearchBoxBtn2 />
+
+      <!--AlgoliaSearchBox v-if="isAlgoliaSearch" :options="algolia" /-->
       <!-- Updated: wkande: There are paths where the SearchBox cannot be added. -->
-      <SearchBox
+      <!--SearchBox
         v-else-if="
           $route.path != '/' &&
           $site.themeConfig.search !== false &&
           $page.frontmatter.search !== false
         "
-      />
+      /-->
       <!-- Added: wkande: Job posting icon. -->
       <api3-JobsIcon class="can-hide" />
 
