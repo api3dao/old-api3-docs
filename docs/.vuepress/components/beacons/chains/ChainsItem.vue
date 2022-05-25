e7c7986dbda0fb0e5b9356381e9b3b5f55b81000
Warren Anderson <warren@api3.org>
2022-05-25 10:43:02 -0600

    added polygon-testnet contract, remove icons

--- docs/.vuepress/components/beacons/chains/ChainsItem.vue
+++ docs/.vuepress/components/beacons/chains/ChainsItem.vue
@@ -4,30 +4,20 @@
 
 <template>
   <div>
-    <div class="bc-chains-row">
-      <!-- LEFT column -->
-      <div class="bc-chains-column-left" style="background-color: black">
-        <img :src="chain.logoPath" width="45px;" />
-      </div>
+    <div class="bc-chains-name">
+      <a :href="chain.explorerUrl">{{ chain.name }}</a
+      ><ExternalLinkImage />
+    </div>
+    <div class="bc-chains-id">Id: {{ chain.id }}</div>
 
-      <!-- RIGHT column -->
-      <div class="bc-chains-column-right">
-        <span style="font-size: x-small"> {{ chain.id }} </span>
-        <span class="bc-chains-name"
-          ><a :href="chain.explorerUrl">{{ chain.name }}</a
-          ><ExternalLinkImage />
-        </span>
-
-        <!-- Contract list -->
-        <div
-          class="bc-chains-contract-address"
-          v-for="(address, key) in chain.contracts"
-          v-bind:key="address"
-        >
-          {{ key }}: <span>{{ address }}</span
-          ><CopyIcon :text="address" />
-        </div>
-      </div>
+    <!-- Contract list -->
+    <div
+      class="bc-chains-contract-address"
+      v-for="(address, key) in chain.contracts"
+      v-bind:key="address"
+    >
+      {{ key }}: <span>{{ address }}</span
+      ><CopyIcon :text="address" />
     </div>
   </div>
 </template>
@@ -40,35 +30,18 @@ export default {
 </script>
 
 <style>
-.bc-chains-row {
-  display: flex;
-  flex-direction: row;
-  width: 100%;
-}
-
-.bc-chains-column-left {
-  display: flex;
-  flex-direction: column;
-  flex: 1;
-  max-width: 50px;
-  min-width: 50px;
-  margin-right: 10px;
-}
-.bc-chains-column-right {
-  display: flex;
-  flex-direction: column;
-  overflow-wrap: anywhere;
-  flex: 1;
-}
-
 .bc-chains-name {
   font-size: large;
   font-weight: bold;
   margin-bottom: 5px;
   cursor: pointer;
 }
+.bc-chains-id {
+  font-size: small;
+}
 .bc-chains-contract-address {
   font-family: courier;
   font-size: small;
+  margin-top: 3px;
 }
 </style>
