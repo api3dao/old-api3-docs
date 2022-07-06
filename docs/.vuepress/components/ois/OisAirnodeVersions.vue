bb9c14982a13f29b54931495e16ae81528f775f7
Warren Anderson <warren@api3.org>
2022-07-06 14:58:24 -0600

    Add latest to version table

--- docs/.vuepress/components/ois/OisAirnodeVersions.vue
+++ docs/.vuepress/components/ois/OisAirnodeVersions.vue
@@ -9,12 +9,15 @@
     deploying.
     <table>
       <th>OIS version</th>
-      <th>Airnode versions</th>
+      <th>OIS latest</th>
+      <th>Compatible Airnode versions</th>
       <tr>
+        <td>1.0.x</td>
         <td>1.0.0</td>
         <td>0.2.x through 0.7.x</td>
       </tr>
       <tr>
+        <td>1.1.x</td>
         <td>1.1.1</td>
         <td>0.8.x</td>
       </tr>
