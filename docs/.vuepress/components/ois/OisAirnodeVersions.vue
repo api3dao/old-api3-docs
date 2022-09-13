23228f782501bd699642f13b188bdf860bb25303
Warren Anderson <warren@api3.org>
2022-09-13 10:39:44 -0600

    Change dependency chart

--- docs/.vuepress/components/ois/OisAirnodeVersions.vue
+++ docs/.vuepress/components/ois/OisAirnodeVersions.vue
@@ -1,6 +1,9 @@
 <!--
   This component displays a list of OIS versions and the Airnodes version they 
   relate to.
+
+  This component must be updated when a new Airnode version is deployed. This
+  is described in the /dev/versions.md page.
 -->
 
 <template>
@@ -8,26 +11,23 @@
     Be sure to use the correct OIS version for the Airnode version you are
     deploying.
     <table>
-      <th>OIS</th>
-      <th>OIS releases</th>
-      <th>Compatible Airnode versions</th>
+      <th>Airnode version</th>
+      <th>Compatible OIS version</th>
       <tr>
-        <td>1.0.x</td>
+        <td>0.2.x through 0.7.x</td>
         <td>
           <a href="https://github.com/api3dao/ois/tree/v1.0.0" target="_blank"
             >1.0.0 <ExternalLinkImage
           /></a>
         </td>
-        <td>0.2.x through 0.7.x</td>
       </tr>
       <tr>
-        <td>1.1.x</td>
+        <td>0.8.x</td>
         <td>
           <a href="https://github.com/api3dao/ois/tree/v1.1.1" target="_blank"
             >1.1.1 <ExternalLinkImage
           /></a>
         </td>
-        <td>0.8.x</td>
       </tr>
     </table>
   </div>
