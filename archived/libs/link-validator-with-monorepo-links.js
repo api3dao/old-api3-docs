ae917c38d994b9f3e420eb344b0febb419a02728
Emanuel Tesar <e.tesarr@gmail.com>
2023-03-13 17:29:28 +0100

    Replace api3.eth.link with api3.eth

--- archived/libs/link-validator-with-monorepo-links.js
+++ archived/libs/link-validator-with-monorepo-links.js
@@ -84,7 +84,7 @@ async function testLink(url, filePath) {
     }
 
     // Sometimes the anchor indicator (#) is in the
-    // path: https://api3.eth.link/#/history/secondary-6
+    // path: https://api3.eth/#/history/secondary-6
     // This is not an anchor, ignore it.
     if (urlAnchor && urlAnchor.indexOf('#/') > -1) {
       urlAnchor = null;
